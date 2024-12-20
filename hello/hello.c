fb829736bf2606b30ffe95e4f1f0e38f3c0f92a5
LUCI0LUP0 <105591961+LIP0LUCI0@users.noreply.github.com>
2024-12-20 16:09:49 +0000

    hello update

--- hello/hello.c
+++ hello/hello.c
@@ -3,8 +3,19 @@
 /* main */
 int main(){
 
+    //variabile di 32 caratteri
+    char name[32];
 
-    printf("Hello World\n\n\n");
+    // richiesta per il nome
+    printf("Enter a name: ");
+
+    //lettura dell'input dello user
+    scanf("%s", name);
+
+    // stampare il messaggio con il nome digitato dallo user
+    printf("\nHello\t%s\n\n\n", name);
+
+    //printf("Hello World\n\n\n");
 
     return 0;
 }
