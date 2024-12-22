d0ccfd4b1159fcf32920d75a7a4ca5d0c618084f
LUCI0LUP0 <105591961+LIP0LUCI0@users.noreply.github.com>
2024-12-22 12:15:18 +0000

    main file

---
+++ compito _lezione01_Caglioti_Luca/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+    
+    char nome[50];
+    char cognome[50];
+
+    printf("Inserisci il tuo nome:\t");
+    scanf("%s", nome);
+
+    printf("\nInserisci il tuo cognome:\t");
+    scanf("%s", cognome);
+
+    printf("\nBenvenuto, %s %s!", nome, cognome);
+    printf("\nGrazie per aver usato il nostro programma.");
+    printf("\n\nTi auguriamo Buone Feste\n   *\n  ***\n *****\n*******\n  ***\n\n\n\n");
+}
